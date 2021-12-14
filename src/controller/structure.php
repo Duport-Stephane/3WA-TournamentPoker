298cfc196b49ce6a4e5466cfbb52c4194d3d3f8f
Duport-Stephane <duportstephane35@gmail.com>
2021-12-14 23:25:49 +0100

    14 12 2021

---
+++ src/controller/structure.php
@@ -0,0 +1,24 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    header('Location: ./index.php?page=home');
+    die;
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+
+    // Test if the user is connected
+    try {
+        if (!\Models\Session::isConnected()) {
+            throw new DomainException('Veuillez vous connecter pour accéder à cette page.');
+        }
+    } catch (DomainException $e) {
+        \Models\Session::setOffset('alert', $e->getMessage());
+        echo $e->getMessage();
+        header('Location: ./index.php?page=home');
+        die;
+    }
+}
+
+require_once './src/views/structure.phtml';
